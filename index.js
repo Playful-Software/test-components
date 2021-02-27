6ff67d422facf90992e913565e7fee2c68d08170
Darrin Massena <darrin@massena.com>
2021-02-27 09:08:36 -0800

    Add index, KitDescription

---
+++ index.js
@@ -0,0 +1,10 @@
+import { CanvasDescription } from "./canvas";
+import { DweetDescription } from "./dweet";
+
+// TODO: export const KitDescription?
+export const kit = {
+  title: "Test Kit",
+  description: "Test Kit contains test components",
+  author: "Playful Software",
+  components: [DweetDescription, CanvasDescription],
+};
