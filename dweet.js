f561eaa9d30ba2d3cc355fc1e06a7d09a655dc67
Darrin Massena <darrin@massena.com>
2021-03-08 19:44:04 -0800

    Apply Prettier

--- dweet.js
+++ dweet.js
@@ -23,12 +23,13 @@ const DweetPrototype = {
     // Remember the default globals so that later we can remove any that
     // were created by the dweet
     this._browserGlobals = {};
-    Object.keys(window).forEach(key => {
+    Object.keys(window).forEach((key) => {
       this._browserGlobals[key] = true;
     });
   },
 
   unmount() {
+    this.play = false;
     this.removeBrowserGlobals();
   },
 
@@ -69,7 +70,7 @@ const DweetPrototype = {
       //let u = eval('function u(t) {\n'+instrument(code)+'\n}');
       this._u = new Function('t,c,x,S,C,T,R', instrument(code)).bind(this);
     } catch (e) {
-      this._u = function() {
+      this._u = function () {
         throw e;
       };
       throw e;
@@ -79,16 +80,16 @@ const DweetPrototype = {
   },
 
   loop(frame_time) {
-    if (this.play){
+    if (this.play) {
       requestAnimationFrame(this.loop.bind(this));
       var epsilon = 1.5; // Acounts for different timestamp resolution and slight jitter
       if (frame_time < this._nextFrameMs - epsilon) {
-        return;  // Skip this cycle as we are animating too quickly.
+        return; // Skip this cycle as we are animating too quickly.
       }
       this._nextFrameMs = Math.max(this._nextFrameMs + 1000 / this._FPS, frame_time);
     }
-    this._time = this._frame/this._FPS;
-    if(this._time * this._FPS | 0 == this._frame - 1){
+    this._time = this._frame / this._FPS;
+    if ((this._time * this._FPS) | (0 == this._frame - 1)) {
       this._time += 0.000001;
     }
     this._frame++;
@@ -98,13 +99,18 @@ const DweetPrototype = {
     const S = Math.sin;
     const C = Math.cos;
     const T = Math.tan;
-    const R = function(r,g,b,a) {
+    const R = function (r, g, b, a) {
       a = a === undefined ? 1 : a;
-      return 'rgba('+(r|0)+','+(g|0)+','+(b|0)+','+a+')';
+      return 'rgba(' + (r | 0) + ',' + (g | 0) + ',' + (b | 0) + ',' + a + ')';
     };
 
     try {
-      if (window.navigator.userAgent.indexOf('Edge') > -1 && (this._u + '').match(/c\s*\.\s*(width|height)\s*(=|\+=|-=|\*=|\/=|%=|\**=|<<=|>>=|>>>=|&=|\^=|\|=)/) != null) {
+      if (
+        window.navigator.userAgent.indexOf('Edge') > -1 &&
+        (this._u + '').match(
+          /c\s*\.\s*(width|height)\s*(=|\+=|-=|\*=|\/=|%=|\**=|<<=|>>=|>>>=|&=|\^=|\|=)/
+        ) != null
+      ) {
         x.beginPath();
         x.resetTransform();
         x.clearRect(0, 0, c.width, c.height);
@@ -119,25 +125,27 @@ const DweetPrototype = {
   },
 
   displayError(e) {
-    if(this._lastError != e){
+    if (this._lastError != e) {
       this._lastError = e;
+      /*
       parent.postMessage({
         'type': 'error',
         'error': ''+e,
         'location': window.location.href
       },'*');
+      */
     }
   },
 
   setupPolyfills(x) {
     if (typeof x.resetTransform === 'undefined') {
-      x.resetTransform = function() {
+      x.resetTransform = function () {
         this.setTransform(1, 0, 0, 1, 0, 0);
       };
     }
 
     if (typeof x.ellipse === 'undefined') {
-      x.ellipse = function(x, y, rx, ry, rotation, startAngle, endAngle, antiClockwise) {
+      x.ellipse = function (x, y, rx, ry, rotation, startAngle, endAngle, antiClockwise) {
         this.save();
         this.translate(x, y);
         this.rotate(rotation);
@@ -150,14 +158,14 @@ const DweetPrototype = {
 
   removeBrowserGlobals() {
     if (this._browserGlobals) {
-      Object.keys(window).forEach(key => {
+      Object.keys(window).forEach((key) => {
         if (!this._browserGlobals[key]) {
           //console.log('Removing new global: ' + key);
           delete window[key];
         }
       });
     }
-  }
+  },
 };
 
 // TODO: anything that can be removed? title?
@@ -178,7 +186,11 @@ export const DweetDescription = {
   properties: {
     // TODO: placeholder: { type: "String", default: "" }?
     link: { type: 'string', default: '' },
-    dweet: { type: 'string', default: 'c.width=2e3;t*=4;x.translate(980,540);for(i=0;i<32;)x.rotate(!i*C(t-=.03)+!(i++%4)*S(t)/9+1.57),x.fillRect(9*i,9*i,99,99)' },
+    dweet: {
+      type: 'string',
+      default:
+        'c.width=2e3;t*=4;x.translate(980,540);for(i=0;i<32;)x.rotate(!i*C(t-=.03)+!(i++%4)*S(t)/9+1.57),x.fillRect(9*i,9*i,99,99)',
+    },
     author: { type: 'string', default: '' },
     // TODO: allow type-inferred simple form ala pause: false?
     play: { type: 'boolean', default: true },
